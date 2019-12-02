2fc8f5b7d12638d37e46fbf83c436e97d2674109
WHU-gentle <1694272297@qq.com>
2019-12-02 17:02:21 +0800

    对功能部件进行了整理，优化了程序结构

--- Game.py
+++ Game.py
@@ -1,10 +1,8 @@
 import os
-import sys
 import pygame
-import pygame.freetype  # 绘制文字的增强库
 import io
 import time
-#from CtlClass import *
+from ctlClass import *
 
 # 游戏资源存储
 pic_bg = []
@@ -12,13 +10,15 @@ pic_art = {}
 article = []
 event = {}
 sentence = {}
+PC = 0  # 处理的语句在article中的位置
 
 # 读取结构化剧本文件
 with open(u"./text/Chapter1.txt", encoding='UTF-8') as data:
     for line in data.readlines():
         if line == '\n':
             continue
-        article.append(str(line).lstrip())
+        article.append(str(line).strip())
+
 num = -1  # 记录行数
 for col in article:
     num = num + 1
@@ -36,99 +36,55 @@ for col in article:
     if '9' >= col[0][0] >= '1':
         sentence[col[0]] = num
 
-# 初始化
-pygame.init()
-# 1.窗体设置
-size = width, height = 1000, 700  # 通过函数传递参数 设置固定的窗口大小
-screen = pygame.display.set_mode(size)    # 创建窗口
-# 2.背景图片的设置
-BLACK = 0, 0, 0  # 通过函数传递参数 设置窗口背景颜色
-screen.fill(BLACK)  # 设置窗口背景颜色
-background = pygame.image.load("./img/bg/bg1.jpg")  # 载入背景图片
-screen.blit(background, (0, 0))  # 把图片background放到（0,0）的位置
-# 3.窗体标题栏的设置
-# 4.矩形框的设置
-#r1POS=0,50,500,100  # r1rect矩形框的左上角坐标x,y,矩形框的宽度，高度
-#r1WIDTH=0   # 矩形的边框宽度。0表示填充矩形
-#r1rect=pygame.draw.rect(screen,GREEN,r1POS,r1WIDTH)
 
-# 5.文字的设置
-BLACK = 0, 0, 0
-WHITE = 255, 255, 255
-GREEN = 95, 171, 84
-micFONT = pygame.freetype.Font('C://Windows//Fonts//simhei.ttf', 18)  # 字体路径，字体大小
-
-
-class Text():
+def LoadInstr():
     """
-    role: 字符串代表角色名字
-    position: 角色位置
-    talk: 角色所说的话
+    取一条文本指令
+    :return: 经过空格分隔后的句子元素列表
     """
-    def __init__(self, role, position, talk):
-        self.role = role
-        self.position = position
-        self.talk = talk
-        self.diag = pygame.image.load("./img/diag_board.jpg")
-        self.role_pic = pygame.image.load(pic_art[self.role])  # 加载图片
-
-    def display(self):
-        choicePOS = [70, 531]  # 对话出现的位置
-        if self.position == '左':
-            role_position = 0
-        elif self.position == '中':
-            role_position = 400
-        elif self.position == '右':
-            role_position = 700
-        screen.blit(self.role_pic, (role_position, 0))  # 显示角色图片
-        screen.blit(self.diag, (40, 500))  # 显示对话背景
-        wordRect1 = micFONT.render_to(screen, choicePOS, self.talk, fgcolor=BLACK, size=30)  # 输出对话内容
-        choicePOS[1] = choicePOS[1] + 30
+    global PC
+    i = article[PC]
+    i = i.split(' ')
+    PC += 1
+    return i
 
 
-# 剧情发展
+R_name = ['' for i in range(3)]
+R_bool = [True for i in range(3)]
 
-for col in article:
-    '''for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # 用户按下了结束键
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            break'''
-    t = 0
-    if '9' >= col[0][0] >= '1':
-        index = sentence[col[0]]
-        sen = article[index]
-        sen = sen.split(' ')
-        TEXT = Text(sen[1], sen[2], sen[3][1: -3])
-        TEXT.display()
-        t = int(len(sen[3])/8)
-    pygame.display.update()  # 显示图片
-    time.sleep(t)
 
-'''
-while True:
+def ParseInstr():
+    """
+    分析指令函数：判断指令是对话还是功能指令，功能指令则执行器操作，对话指令返回内容进行显示
+    :return: 四个变量分别为左中右需要显示的人物图片，及对话内容
+    """
+    global R_bool, R_name
+    while PC < len(article):
+        ins = LoadInstr()  # 取一条指令
+        if ins[0] == 'set':  # 功能扩展
+            if ins[1] == 'position':  # 设置人物的显示
+                for k in range(2, 5):
+                    if ins[k] == 'null':
+                        R_name[k-2] = ''
+                        R_bool[k-2] = False
+                    else:
+                        R_name[k-2] = ins[k]
+                        R_bool[k-2] = True
+        elif '1' <= ins[0][0] <= '9':
+            return ins[3][1:-3]  # 只有遇到说话内容的时候才返回
+
+
+# 控制流程
+while True:  # 无限循环直至用户点击×
+    if PC >= len(article):
+        Speaker('', False, '', False, '', False, "本章已完结……")
+        sys.exit()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # 用户按下了结束键
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            text = Text(1, 0,file_dialog)
-        # 说明：用户输入A/B/C选择选项
-        elif event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_a :
-                print("A choice")
-                text = Text(2, 1,option1)
-            elif event.key==pygame.K_b:
-                print("B choice")
-                text = Text(3, 2,option2)
-            elif event.key==pygame.K_c:
-                print("C choise")
-                text = Text(4, 2,option3)
-        elif event.type==pygame.MOUSEMOTION:    # 设计时用鼠标获得当前位置，定位用
-            print("[MOUSEMOTION]:",event.pos,event.rel,event.buttons)
-        elif event.type==pygame.MOUSEBUTTONUP:  # 设计时用鼠标获得当前位置，定位用
-            print("[MOUSEBUTTONUP]:",event.pos,event.button)
-        elif event.type==pygame.MOUSEBUTTONDOWN:    # 设计时用鼠标获得当前位置，定位用
-            print("[MOUSEBUTTONDOWN]:",event.pos,event.button)
-    pygame.display.update() # 显示图片
-'''
-
+            t = ParseInstr()
+            n = R_name
+            b = R_bool
+            Speaker(n[0], b[0], n[1], b[1], n[2], b[2], t)
+    pygame.display.update()  # 显示图片
